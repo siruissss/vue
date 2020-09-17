21d33f1fb1ee69d6857d11eff7edc504db3c5706
zoe <854228841@qq.com>
2020-09-17 20:54:56 +0800

    commit zoe-vue

--- zoe-vue/src/main.js
+++ zoe-vue/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 //反向代理
 var axios = require('axios')
@@ -11,9 +13,12 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
